1adc3d943bfee876ecc24b81315b74e0fb5760bb
Tomas David Ye <tomas@tomasdavidye.tech>
2022-09-17 18:36:33 -0700

    [VIDEO 1 -> STOCK PRICES] 1. Finished coding of the video

--- stock_price/get_stock_prices.py
+++ stock_price/get_stock_prices.py
@@ -1,5 +1,17 @@
+import yfinance as yf
+from matplotlib import pyplot as plt
+
+NUM_DAYS = 60
+STOCK_SYMBOL = 'GOOG'
+
+
 def main():
-    print('Hello World!')
+    stock = yf.Ticker(STOCK_SYMBOL)
+    stock_data = stock.history(period=f'{NUM_DAYS}d')
+    stock_data['Open'].plot()
+
+    plt.title(f'{STOCK_SYMBOL} Price in the Last {NUM_DAYS} Days')
+    plt.show()
 
 
 if __name__ == '__main__':
